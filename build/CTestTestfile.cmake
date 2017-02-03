92fdc64614b65f4c021e307808f38f75f5f1c06f
Mana4 <abdelrhman.ali75@gmail.com>
2017-02-03 22:50:53 +0200

    upload smart_car work sapce after build it

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mana/smart_car/src
+# Build directory: /home/mana/smart_car/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
